d16f6d6990de9f656c066b782825ca4affe7c0a7
Aman Kumar <amankumar@Amans-MacBook-Air-4.local>
2026-02-28 21:09:13 +0530

    Update changes

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,44 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//     experimental: {
+//         serverActions: {
+//             bodySizeLimit: '100mb',
+//         }
+//     },
+//     typescript: {
+//         ignoreBuildErrors: true,
+//     },
+//     images: { remotePatterns: [
+//             { protocol: 'https', hostname: 'covers.openlibrary.org' },
+//             { protocol: 'https', hostname: 'lspfdyhgsrgsxcju.public.blob.vercel-storage.com' },
+//         ]}
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        serverActions: {
-            bodySizeLimit: '100mb',
-        }
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb",
     },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images: { remotePatterns: [
-            { protocol: 'https', hostname: 'covers.openlibrary.org' },
-            { protocol: 'https', hostname: 'lspfdyhgsrgsxcju.public.blob.vercel-storage.com' },
-        ]}
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "covers.openlibrary.org",
+      },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
